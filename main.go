7832f4fbdd7c8beea121a027dab94e9a431d40d1
Tejas Dixit <pandatd@pop-os.localdomain>
2023-03-30 13:34:29 +0530

    added support for the Int input

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package requirements
 
+// Author: github.com/pandatd/
+// License: MIT
+// Description: This is a simple package for taking input from the user and all usefull funcations for daily use in Golang devlopment.
+// Package : github.com/pandatd/requirements@latest.
+// version : v1.1.2
+// contact:	+91 7745014673
+// mail : tejasdixit17@proton.me
+
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
@@ -11,6 +20,22 @@ func GetStringInput() string {
 	//Logic for Taking String Input
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
+
+	//Error Handling Part
+
+	if err != nil {
+		log.Println("Oh Ohh Something Went WRONG ")
+	}
+	// Ruturning the input
+	return input
+
+}
+
+func GetIntInput() int {
+
+	//Logic for Taking Int Input
+	var input int
+	_, err := fmt.Scan(&input)
 
 	//Error Handling Part
 
